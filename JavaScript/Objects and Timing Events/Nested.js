1006cc8e945f946a3f3a7368c97d3c5cc6009048
Harish Nishad <harish.nishad0775@gmail.com>
2020-12-17 00:38:41 +0530

    file Commited :   new file:   JavaScript/Objects and Timing Events/Array_r_object_.js         new file:   JavaScript/Objects and Timing Events/Nested.js         new file:   JavaScript/Objects and Timing Events/Objects.js         new file:   JavaScript/Objects and Timing Events/TimeEvent.js         new file:   JavaScript/Objects and Timing Events/time_Counter.js

---
+++ JavaScript/Objects and Timing Events/Nested.js
@@ -0,0 +1,31 @@
+var student = {
+	name : "Harish Nishad",
+	roll_No : "186514628",
+	Marks : "90",
+	address : {
+		City : " New Delhi",
+		PinCode : 111095,
+	  Country : "India"
+	}
+}
+
+
+/*
+keys
+(3) ["name", "roll_No", "Marks"]
+student
+{name: "Harish Nishad", roll_No: "186514628", Marks: "90", address: {…}}
+Marks: "90"address: City: " New Delhi"Country: "India"PinCode: 111095__proto__: Objectname: "Harish Nishad"roll_No: "186514628"__proto__: Object
+student.address.City = "AGRA"
+"AGRA"
+student
+{name: "Harish Nishad", roll_No: "186514628", Marks: "90", address: {…}}
+Marks: "90"address: {City: "AGRA", PinCode: 111095, Country: "India"}name: "Harish Nishad"roll_No: "186514628"__proto__: Object
+
+
+*/
+
+
+
+
+// ..............................Ayay
